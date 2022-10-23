2819dd53714bcf8317b7d534a8ab4dd85652e963
jared <jaredamcdon@gmail.com>
2022-10-23 17:35:36 -0400

    adding some basic boilerplate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+
+#[get("/ping")]
+async fn ping() -> impl Responder{
+    println!("You have been pinged");
+    HttpResponse::Ok().body("pong")
+}
+#[get("/")]
+async fn index() -> impl Responder{
+    HttpResponse::Ok().body("Grist Index")
+}
+
+// git commands
+
+#[get("/init")]
+async fn git_init() -> impl Responder{
+    println!("git init");
+    HttpResponse::Ok().body("repo init")
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(
+                web::scope("/api")
+                    .service(ping)
+                    .service(index)
+                    .service(
+                        web::scope("/git")
+                        .service(git_init)
+                    )
+            )
+    })
+    .bind("0.0.0.0:7000")?
+    .run()
+    .await
 }
