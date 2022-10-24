6c34b3581d5d98f75cb3134c85c36d45b29c54ca
jared <jaredamcdon@gmail.com>
2022-10-24 01:29:56 -0400

    adding beginning functions

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
 
+// structs etc
+//
+// this is what i used for the base https://medium.com/swlh/build-your-first-rest-api-using-rust-language-and-actix-framework-8f827175b30f
+
 #[get("/ping")]
 async fn ping() -> impl Responder{
     println!("You have been pinged");
@@ -12,11 +16,26 @@ async fn index() -> impl Responder{
 
 // git commands
 
-#[get("/init")]
-async fn git_init() -> impl Responder{
-    println!("git init");
+#[get("/init/{name}")]
+async fn git_init(repo: web::Path<String>) -> impl Responder{
+    //println!("{}", repo.to_string());
+    let repo_name = String::from(repo.as_str());
+    println!("git init for: {}", repo_name);
     HttpResponse::Ok().body("repo init")
 }
+
+#[get("/verify/{name}")]
+//async fn git_verify(repo: web::Path<String>) -> impl Responder {
+async fn git_verify(repo: web::Path<String>) -> HttpResponse {
+    let mut repo_name = String::from(repo.as_str());
+    println!("this will eventually check for git repo: {}", repo_name);
+    //let output: String = ("{} doesnt exist this is scaffolding code", repo_name.as_str());
+    repo_name.push_str(" doest exist this is scaffolding code");
+    //HttpResponse::Ok().body(output.to_string())
+    HttpResponse::Ok().body(repo_name)
+}
+
+//end git commands
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
@@ -29,6 +48,7 @@ async fn main() -> std::io::Result<()> {
                     .service(
                         web::scope("/git")
                         .service(git_init)
+                        .service(git_verify)
                     )
             )
     })
